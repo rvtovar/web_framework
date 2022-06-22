b24405dc96276adfc7028c9e84a4f9cdfcfcadb0
Rose Tovar <roseatovar@gmail.com>
2022-06-22 09:13:04 -0500

    created a user class with a get and set method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hi There');
+import { User } from './models/User';
+
+const user = new User({ name: 'Rose', age: 31 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+user.set({
+  name: 'skye',
+});
+
+console.log(user.get('name'));
+console.log(user.get('age'));
